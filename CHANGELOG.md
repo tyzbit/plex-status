a7e7f98cbb52fed23b2ce1eaaab5c087ed1b79e6
tyzbit <tyzbit@gmail.com>
2017-10-17 00:15:42 -0400

    better "docker stop" handling

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+plex-status CHANGELOG
+=====================
+
+>10/17/2017 - Better handling of "docker stop"
+
+>10/15/2017 - Initial commit
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 while true; do
   node /plex-status.js
-  sleep 60
+  sleep 60 &
+  pid=$?
+  trap "kill $pid" SIGTERM
+  wait
 done
